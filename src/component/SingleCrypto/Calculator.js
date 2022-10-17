c1305c1561ebf6695652abd292c8b5f1cd6a42f7
Abdul Alim <abdulalimrakib53@gmail.com>
2022-10-17 18:16:38 +0600

    calculator

--- src/component/SingleCrypto/Calculator.js
+++ src/component/SingleCrypto/Calculator.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useCurrencies from '../../hooks/useCureencies';
+import SelectOption from '../ui/SelectOption';
 
 const Calculator = () => {
+  const currency = useCurrencies();
+  const [currencies, setCurrencies] = useState([currency]);
+  const [selectedOption, setSelectedOption] = useState(currency);
+  const [inputValueOne, setInputValueOne] = useState(1);
+  const [resultValue, setResultValue] = useState()
+  const handleSelect = (e) => {
+    setSelectedOption(e);
+  };
+
   return (
     <div>
       <div className="flex flex-col gap-5">
@@ -9,6 +20,36 @@ const Calculator = () => {
           Use the calculator to convert real-time prices between all available
           cryptocurrencies and fiat..
         </p>
+        <form className="flex flex-col gap-5 w-full">
+          <div className="grid grid-cols-3 border border-brand rounded overflow-hidden">
+            <input
+              type="number"
+              value="1"
+              className="col-span-2 bg-transparent focus:outline-none hover:outline-none px-3"
+            />
+            <SelectOption
+              options={currencies}
+              placeholder="Team"
+              value={selectedOption}
+              onChange={(e) => handleSelect(e)}
+              required
+            />
+          </div>
+          <div className="grid grid-cols-3 border border-brand rounded overflow-hidden">
+            <input
+              type="number"
+              value="1"
+              className="col-span-2 bg-transparent focus:outline-none hover:outline-none px-3"
+            />
+            <SelectOption
+              options={currencies}
+              placeholder="Team"
+              value={selectedOption}
+              onChange={(e) => handleSelect(e)}
+              required
+            />
+          </div>
+        </form>
       </div>
     </div>
   );
