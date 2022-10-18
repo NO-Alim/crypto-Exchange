032c0d5d2ce1fa957d12a374def69079ef0533d8
Abdul Alim <abdulalimrakib53@gmail.com>
2022-10-19 02:23:40 +0600

    news page done

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,11 +1,41 @@
 import React from 'react';
-
+import { useGetCryptoNewsQuery } from '.././features/cryptoNews/cryptoNewsApi';
+import NewsCart from '../component/Home/NewsCart';
+import Error from '../component/ui/Error';
+import Loader from '../component/ui/Loader';
 const News = () => {
-  return (
-    <div className="section bg-background/90 min-h-screen text-textPrimary">
-      News
-    </div>
-  );
+  const { data, isLoading, isError, error } = useGetCryptoNewsQuery({
+    newsCategory: 'crypto',
+    count: 10,
+  });
+
+  let content;
+  if (isLoading)
+    content = (
+      <div className="grid grid-cols-2 sm:grid-cols-3 gap-5">
+        <div className="col-span-1">
+          <Loader />
+        </div>
+        <div className="col-span-1">
+          <Loader />
+        </div>
+        <div className="col-span-1">
+          <Loader />
+        </div>
+      </div>
+    );
+  if (!isLoading && isError) content = <Error message={error.error} />;
+  if (data?.value?.length > 0) {
+    const newses = data.value;
+    content = (
+      <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5">
+        {newses.map((item, ind) => {
+          return <NewsCart item={item} key={ind} />;
+        })}
+      </div>
+    );
+  }
+  return <div className="bg-background section min-h-screen">{content}</div>;
 };
 
 export default News;
