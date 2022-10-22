8e660db948fc644106f85128d0614146666ddaeb
Abdul Alim <abdulalimrakib53@gmail.com>
2022-10-22 08:48:52 +0600

    gh-pages deploy

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from './component/Footer';
 import Navbar from './component/Navbar';
+import FourOFour from './pages/FourOFour';
 import Home from './pages/Home';
 import News from './pages/News';
 import Ranking from './pages/Ranking';
@@ -15,6 +16,7 @@ function App() {
         <Route path="/ranking" element={<Ranking />} />
         <Route path="/newses" element={<News />} />
         <Route path="/crypto/:cryptoId" element={<SingleCrypto />} />
+        <Route path="*" element={<FourOFour />} />
       </Routes>
       <Footer />
     </Router>
